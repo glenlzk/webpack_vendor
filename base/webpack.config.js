c3eb0efb22a8168d0b6b746e9c2b33e35a3f29be
glen <512043920@qq.com>
2017-09-01 16:01:15 +0800

    vendor manifest

---
+++ base/webpack.config.js
@@ -0,0 +1,17 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+	entry: './app/index.js',
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+	plugins:[
+		new webpack.optimize.UglifyJsPlugin({
+		    compress: {
+		      warnings: false,
+		    }
+		}),
+	]
+}
